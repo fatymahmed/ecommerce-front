7c855f7a4f860499ce0622b52a48c6b82486565c
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-07 20:32:20 +0300

    Added private route for authenticated users only

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -19,7 +19,15 @@ const Menu = ({ history }) => {
             Home
           </Link>
         </li>
-
+        <li className='nav-item'>
+          <Link
+            className='nav-link'
+            style={isActive(history, "/dashboard")}
+            to='/dashboard'
+          >
+            Dashboard
+          </Link>
+        </li>
         {!isAuthenticated() && (
           <Fragment>
             {" "}
---
+++ src/user/userDashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+const DashBoard = () => {
+  return <Layout title='Dashboard' description='User Dashboard'></Layout>;
+};
+
+export default DashBoard;
