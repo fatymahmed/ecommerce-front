4fbf71d0b14c2b9cbac0dc89bf6b87b9ad4ce3dc
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-07 19:52:57 +0300

    Implemented Signin with Localstorage

--- src/auth/index.js
+++ src/auth/index.js
@@ -17,3 +17,28 @@ export const signup = (user) => {
       console.log(err);
     });
 };
+
+export const signin = (user) => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
