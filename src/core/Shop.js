a70aa5c95cd311f9b3a41ed89434233f4bff6bd6
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-11 20:04:21 +0300

    Add load more button in shop

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -14,6 +14,7 @@ const Shop = () => {
   const [error, setError] = useState(false);
   const [limit, setLimit] = useState(6);
   const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(0);
   const [filteredResults, setFilteredResults] = useState([]);
 
   const init = () => {
@@ -31,11 +32,36 @@ const Shop = () => {
       if (data.error) {
         setError(data.error);
       } else {
-        setFilteredResults(data);
+        setFilteredResults(data.data);
+        setSize(data.size);
+        setSkip(0);
       }
     });
   };
 
+  const loadMore = () => {
+    let toSkip = skip + limit;
+    getFilteredProducts(toSkip, limit, myFilters.filters).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults([...filteredResults, ...data.data]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 &&
+      size >= limit && (
+        <button onClick={loadMore} className='btn btn-warning mb-5'>
+          Load more
+        </button>
+      )
+    );
+  };
   useEffect(() => {
     init();
     loadFilteredResults(skip, limit, myFilters.filters);
@@ -88,7 +114,16 @@ const Shop = () => {
             />
           </div>{" "}
         </div>
-        <div className='col-8'>{JSON.stringify(filteredResults)}</div>
+        <div className='col-8'>
+          <h2 className='mb-4'>Products</h2>
+          <div className='row'>
+            {filteredResults.map((product, i) => (
+              <Card key={i} product={product} />
+            ))}
+          </div>
+          <hr />
+          {loadMoreButton()}
+        </div>
       </div>
     </Layout>
   );
