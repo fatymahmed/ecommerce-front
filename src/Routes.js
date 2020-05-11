61e0247e3022a49503230afb8bb4b2d90eecb81b
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-11 10:51:31 +0300

    Implemented create product

--- src/Routes.js
+++ src/Routes.js
@@ -8,6 +8,7 @@ import AdminRoute from "./auth/AdminRoute";
 import DashBoard from "./user/UserDashboard";
 import AdminDashBoard from "./user/AdminDashboard";
 import AddCategory from "./admin/AddCategory";
+import AddProduct from "./admin/AddProduct";
 
 const Routes = () => {
   return (
@@ -19,6 +20,7 @@ const Routes = () => {
         <PrivateRoute path='/user/dashboard' exact component={DashBoard} />
         <AdminRoute path='/admin/dashboard' exact component={AdminDashBoard} />
         <AdminRoute path='/create/category' exact component={AddCategory} />
+        <AdminRoute path='/create/product' exact component={AddProduct} />
       </Switch>
     </BrowserRouter>
   );
