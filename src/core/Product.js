c2bd9bce80595b0a5e6a2467ef73f1d0a6a9bc33
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-12 17:35:29 +0300

    Added related products under product's detail page

--- src/core/Product.js
+++ src/core/Product.js
@@ -1,26 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { read } from "./apiCore";
+import { read, listRelated } from "./apiCore";
 import Card from "./Card";
 
 const Product = (props) => {
   const [product, setProduct] = useState({});
+  const [relatedProducts, setRelatedProducts] = useState([]);
   const [error, setError] = useState(false);
 
-  const loadingSingleProduct = (productId) => {
+  const loadSingleProduct = (productId) => {
     read(productId).then((data) => {
       if (data.error) {
         setError(data.error);
       } else {
         setProduct(data);
+        listRelated(data._id).then((data) => {
+          if (data.error) {
+            setError(data.error);
+          } else {
+            setRelatedProducts(data);
+          }
+        });
       }
     });
   };
 
   useEffect(() => {
     const productId = props.match.params.productId;
-    loadingSingleProduct(productId);
-  }, []);
+    loadSingleProduct(productId);
+  }, [props]);
 
   return (
     <Layout
@@ -31,9 +39,20 @@ const Product = (props) => {
       className='container-fluid'
     >
       <div className='row'>
-        {product && product.description && (
-          <Card product={product} showViewProductButton={false} />
-        )}
+        <div className='col-8'>
+          {product && product.description && (
+            <Card product={product} showViewProductButton={false} />
+          )}
+        </div>
+
+        <div className='col-4'>
+          <h4>Related Products</h4>
+          {relatedProducts.map((product, i) => (
+            <div className='mb-3'>
+              <Card key={i} product={product} />
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
   );
