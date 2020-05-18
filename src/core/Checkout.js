744581a0faef265b4a5c7446ca5d0180389ccce3
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-18 21:01:59 +0300

    Create orders

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,31 +1,74 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { getProducts } from "./apiCore";
+import { getProducts, createOrder } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 import { emptyCart } from "./cartHelpers";
 
 const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
+  const [data, setData] = useState({
+    success: false,
+    instance: {},
+    address: "",
+  });
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
     }, 0);
   };
 
-  const showSuccess = () => (
-    <div className='alert alert-info' style={{ display: "" }}>
-      Thank you for shopping with us, your order will be confirmed shortly
+  const showSuccess = (success) => (
+    <div
+      className='alert alert-info'
+      style={{ display: success ? "" : "none" }}
+    >
+      Thanks! Your payment was successful!
     </div>
   );
 
   const handleCheckout = () => {
+    const createOrderData = {
+      products,
+      amount: getTotal(),
+      address: data.address,
+    };
+    createOrder(userId, token, createOrderData);
     emptyCart(() => {
       setRun(!run);
       console.log("payment success and empty cart");
-      showSuccess();
+      setData({
+        success: true,
+      });
     });
   };
+
+  const handleAddress = (event) => {
+    setData({ ...data, address: event.target.value });
+  };
+
+  const showDropIn = () => (
+    <div>
+      {products.length > 0 ? (
+        <div>
+          <div className='gorm-group mb-3'>
+            <label className='text-muted'>Delivery address:</label>
+            <textarea
+              className='form-control'
+              onChange={handleAddress}
+              value={data.address}
+              placeholder='Type your delivery address here...'
+            />
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 
   const showCheckout = () => {
     return isAuthenticated() ? (
@@ -41,7 +84,9 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
 
   return (
     <div>
+      {showDropIn()}
       <h2>Total: ${getTotal()}</h2>
+      {showSuccess(data.success)}
       {showCheckout()}
     </div>
   );
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -72,3 +72,19 @@ export const listRelated = (productId) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const createOrder = (userId, token, createOrderData) => {
+  return fetch(`${API}/order/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ order: createOrderData }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
