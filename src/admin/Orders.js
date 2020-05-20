84f7d704b1ab2cd0cbd4a92c8993d4958298ae72
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-20 15:15:32 +0300

    Allow admin to update order's status

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
-import { listOrders } from "./apiAdmin";
+import { listOrders, getStatusValues, updateOrderStatus } from "./apiAdmin";
 import moment from "moment";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [statusValues, setStatusValues] = useState([]);
 
   const { user, token } = isAuthenticated();
 
@@ -20,8 +21,19 @@ const Orders = () => {
     });
   };
 
+  const loadStatusValues = () => {
+    getStatusValues(user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setStatusValues(data);
+      }
+    });
+  };
+
   useEffect(() => {
     loadOrders();
+    loadStatusValues();
   }, []);
 
   const showOrdersLength = () => {
@@ -43,6 +55,33 @@ const Orders = () => {
     </div>
   );
 
+  const handleStatusChange = (e, orderId) => {
+    updateOrderStatus(user._id, token, orderId, e.target.value).then((data) => {
+      if (data.error) {
+        console.log("Status update failed");
+      } else {
+        loadOrders();
+      }
+    });
+  };
+
+  const showStatus = (o) => (
+    <div className='form group'>
+      <h3 className='mark mb-4'>Status: {o.status}</h3>
+      <select
+        className='form-control'
+        onChange={(e) => handleStatusChange(e, o._id)}
+      >
+        <option>Update Status</option>
+        {statusValues.map((status, index) => (
+          <option key={index} value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
   return (
     <Layout title='Orders' description={`you can manage all the orders here`}>
       <div className='row'>
@@ -59,7 +98,7 @@ const Orders = () => {
                   <span className='bg-primary'>Order ID: {order._id}</span>
                 </h2>
                 <ul className='list-group mb-2'>
-                  <li className='list-group-item'>{order.status}</li>
+                  <li className='list-group-item'>{showStatus(order)}</li>
                   <li className='list-group-item'>Amount: {order.amount}</li>
                   <li className='list-group-item'>
                     Ordered by: {order.user.name}
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -60,3 +60,33 @@ export const listOrders = (userId, token) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const getStatusValues = (userId, token) => {
+  return fetch(`${API}/order/status-values/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+  return fetch(`${API}/order/${orderId}/status/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ status, orderId }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
